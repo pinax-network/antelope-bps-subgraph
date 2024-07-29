72aea35da4a81779efc47f3568f522c4dd9640de
Yaro Shkvorets <shkvorets@gmail.com>
2024-07-29 04:44:30 +0000

    remove unneeded protobuf code

--- substreams/src/pb/mod.rs
+++ substreams/src/pb/mod.rs
@@ -8,14 +8,3 @@ pub mod antelope {
         }
     }
 }
-pub mod sf {
-    pub mod antelope {
-        pub mod r#type {
-            // @@protoc_insertion_point(attribute:sf.antelope.type.v1)
-            pub mod v1 {
-                include!("sf.antelope.type.v1.rs");
-                // @@protoc_insertion_point(sf.antelope.type.v1)
-            }
-        }
-    }
-}
